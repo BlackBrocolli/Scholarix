9d6a3b248718af059e47aa47102909fe7c136f64
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-07 15:11:18 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/stiki/doleno/scholarix/MainActivity.kt
@@ -0,0 +1,46 @@
+package id.ac.stiki.doleno.scholarix
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import id.ac.stiki.doleno.scholarix.ui.theme.ScholarixTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScholarixTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Scholarix")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ScholarixTheme {
+        Greeting("Android")
+    }
+}
