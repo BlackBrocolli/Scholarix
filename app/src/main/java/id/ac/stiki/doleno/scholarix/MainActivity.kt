2d831d06f6cb53a06edb7934a3a5aa6e48fe9a2f
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-12 11:42:54 +0700

    Add onboarding screen

--- app/src/main/java/id/ac/stiki/doleno/scholarix/MainActivity.kt
+++ app/src/main/java/id/ac/stiki/doleno/scholarix/MainActivity.kt
@@ -1,15 +1,35 @@
 package id.ac.stiki.doleno.scholarix
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import id.ac.stiki.doleno.scholarix.ui.theme.ScholarixTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Scholarix")
+                    OnBoardingScreen()
                 }
             }
         }
@@ -30,17 +50,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun OnBoardingScreen() {
+    val context = LocalContext.current
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.onboarding_bg),
+            contentDescription = "Onboarding background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Selamat datang di Scholarix!",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color.White
+                )
+                Text(
+                    text = "Mari kita mulai dengan menyesuaikan pencarian beasiswa sesuai dengan kebutuhan Anda",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 24.dp),
+                    color = Color.White
+                )
+            }
+            Button(
+                onClick = {
+                    Toast.makeText(context, "Ayo mulai!", Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF4A5391))
+            ) {
+                Text(text = "MULAI", fontSize = 16.sp, color = Color.White)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ScholarixTheme {
-        Greeting("Android")
+        OnBoardingScreen()
     }
 }
