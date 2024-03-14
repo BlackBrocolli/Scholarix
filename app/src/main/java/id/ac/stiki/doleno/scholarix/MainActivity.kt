b5accfc8ee3a454047a8aa17c9065cfe3a251aa2
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-14 20:01:22 +0700

    Update onboarding screen & add google fonts

--- app/src/main/java/id/ac/stiki/doleno/scholarix/MainActivity.kt
+++ app/src/main/java/id/ac/stiki/doleno/scholarix/MainActivity.kt
@@ -4,33 +4,31 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import id.ac.stiki.doleno.scholarix.ui.theme.ScholarixTheme
+import id.ac.stiki.doleno.scholarix.ui.theme.openSansFontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,48 +50,60 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun OnBoardingScreen() {
     val context = LocalContext.current
-    Box(modifier = Modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(id = R.drawable.onboarding_bg),
-            contentDescription = "Onboarding background",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.matchParentSize()
-        )
-
-        Column(
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.height(40.dp))
+            Text(
+                text = "Selamat datang di Scholarix",
+                fontSize = 22.sp,
+                fontWeight = FontWeight(600)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Mari kita mulai dengan menyesuaikan pencarian beasiswa sesuai dengan kebutuhan Anda",
+                modifier = Modifier.width(300.dp),
+                textAlign = TextAlign.Center,
+                fontFamily = openSansFontFamily,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.Gray,
+                style = TextStyle(
+                    lineHeight = 20.sp
+                )
+            )
+//            Image(painter = painterResource(id = R.drawable.un), contentDescription = )
+        }
+        Button(
+            onClick = {
+                Toast.makeText(context, "Ayo mulai!", Toast.LENGTH_SHORT).show()
+            },
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .height(48.dp),
+//            colors = ButtonDefaults.buttonColors(Color(0xFF4A5391))
         ) {
-            Box(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = "Selamat datang di Scholarix!",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight(600),
-                    color = Color.White
-                )
-                Text(
-                    text = "Mari kita mulai dengan menyesuaikan pencarian beasiswa sesuai dengan kebutuhan Anda",
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .padding(bottom = 24.dp),
-                    color = Color.White
-                )
-            }
-            Button(
-                onClick = {
-                    Toast.makeText(context, "Ayo mulai!", Toast.LENGTH_SHORT).show()
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp),
-                colors = ButtonDefaults.buttonColors(Color(0xFF4A5391))
-            ) {
-                Text(text = "MULAI", fontSize = 16.sp, color = Color.White)
-            }
+            Text(text = "Lanjutkan", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        TextButton(
+            onClick = {
+                Toast.makeText(context, "Nanti saja!", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+//            colors = ButtonDefaults.buttonColors(Color(0xFF4A5391))
+        ) {
+            Text(
+                text = "Tambahkan preferensi nanti",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
