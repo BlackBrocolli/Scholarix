055dc1df7b910502e9500c2fca995182c614fbd0
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-16 19:51:10 +0700

    Add third onboarding screen

--- app/src/main/java/id/ac/stiki/doleno/scholarix/OnboardingScreen.kt
+++ app/src/main/java/id/ac/stiki/doleno/scholarix/OnboardingScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -48,10 +49,16 @@ import androidx.compose.ui.unit.sp
 import id.ac.stiki.doleno.scholarix.ui.theme.ScholarixTheme
 import id.ac.stiki.doleno.scholarix.ui.theme.poppinsFontFamily
 
+var screenIndex by mutableStateOf(0)
+
 @Composable
 fun OnBoardingScreen() {
-//    FirstOnboarding()
-    SecondOnboarding()
+    when (screenIndex) {
+        0 -> FirstOnboarding()
+        1 -> SecondOnboarding()
+        2 -> ThirdOnboarding()
+//        3 -> FourthOnBoarding()
+    }
 }
 
 @Composable
@@ -91,7 +98,7 @@ fun FirstOnboarding() {
         }
         Button(
             onClick = {
-                Toast.makeText(context, "Ayo mulai!", Toast.LENGTH_SHORT).show()
+                screenIndex++
             },
             modifier = Modifier
                 .fillMaxWidth()
@@ -137,7 +144,7 @@ fun SecondOnboarding() {
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { screenIndex-- }) {
                     Icon(
                         imageVector = Icons.Default.ArrowBack,
                         contentDescription = "Icon arrow back"
@@ -185,7 +192,9 @@ fun SecondOnboarding() {
                         .border(
                             border = BorderStroke(
                                 1.dp,
-                                color = if (selectedRows[0]) Color(0xFF405E90) else Color.Gray.copy(alpha = 0.5f)
+                                color = if (selectedRows[0]) Color(0xFF405E90) else Color.Gray.copy(
+                                    alpha = 0.5f
+                                )
                             ),
                             shape = RoundedCornerShape(20)
                         )
@@ -229,7 +238,9 @@ fun SecondOnboarding() {
                         .border(
                             border = BorderStroke(
                                 1.dp,
-                                color = if (selectedRows[1]) Color(0xFF405E90) else Color.Gray.copy(alpha = 0.5f)
+                                color = if (selectedRows[1]) Color(0xFF405E90) else Color.Gray.copy(
+                                    alpha = 0.5f
+                                )
                             ),
                             shape = RoundedCornerShape(20)
                         )
@@ -273,7 +284,9 @@ fun SecondOnboarding() {
                         .border(
                             border = BorderStroke(
                                 1.dp,
-                                color = if (selectedRows[2]) Color(0xFF405E90) else Color.Gray.copy(alpha = 0.5f)
+                                color = if (selectedRows[2]) Color(0xFF405E90) else Color.Gray.copy(
+                                    alpha = 0.5f
+                                )
                             ),
                             shape = RoundedCornerShape(20)
                         )
@@ -317,7 +330,9 @@ fun SecondOnboarding() {
                         .border(
                             border = BorderStroke(
                                 1.dp,
-                                color = if (selectedRows[3]) Color(0xFF405E90) else Color.Gray.copy(alpha = 0.5f)
+                                color = if (selectedRows[3]) Color(0xFF405E90) else Color.Gray.copy(
+                                    alpha = 0.5f
+                                )
                             ),
                             shape = RoundedCornerShape(20)
                         )
@@ -354,6 +369,173 @@ fun SecondOnboarding() {
                         }
                     }
                 }
+                // == AKHIR PILIHAN LEVEL PENDIDIKAN ==
+            }
+            Button(
+                onClick = {
+                    screenIndex++
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+            ) {
+                Text(
+                    text = "Selanjutnya",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = poppinsFontFamily
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ThirdOnboarding() {
+    val context = LocalContext.current
+    var progress by remember { mutableFloatStateOf(0.66f) }
+
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(onClick = { screenIndex-- }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Icon arrow back"
+                    )
+                }
+                LinearProgressIndicator(
+                    progress = progress,
+                    modifier = Modifier.width(160.dp)
+                )
+                TextButton(onClick = {
+                    Toast.makeText(context, "Nanti saja!", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text(text = "Skip")
+                }
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Tipe Pendanaan",
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Black
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+
+                // == PILIHAN LEVEL PENDIDIKAN ==
+                var selectedRows by remember { mutableStateOf(List(4) { false }) }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                            border = BorderStroke(
+                                1.dp,
+                                color = if (selectedRows[0]) Color(0xFF405E90) else Color.Gray.copy(
+                                    alpha = 0.5f
+                                )
+                            ),
+                            shape = RoundedCornerShape(20)
+                        )
+                        .height(64.dp)
+                        .clickable {
+                            selectedRows = selectedRows
+                                .toMutableList()
+                                .also {
+                                    it[0] = !it[0]
+                                }
+                        }
+                        .background(
+                            shape = RoundedCornerShape(20),
+                            color = if (selectedRows[0]) Color(0xFF405E90) else Color.Transparent
+                        ),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row {
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            text = "Didanai Sepenuhnya",
+                            fontWeight = FontWeight.Bold,
+                            color = if (selectedRows[0]) Color.White else Color.Black
+                        )
+                    }
+                    if (selectedRows[0]) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Icon Check",
+                                tint = Color.White
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                            border = BorderStroke(
+                                1.dp,
+                                color = if (selectedRows[1]) Color(0xFF405E90) else Color.Gray.copy(
+                                    alpha = 0.5f
+                                )
+                            ),
+                            shape = RoundedCornerShape(20)
+                        )
+                        .height(64.dp)
+                        .clickable {
+                            selectedRows = selectedRows
+                                .toMutableList()
+                                .also {
+                                    it[1] = !it[1]
+                                }
+                        }
+                        .background(
+                            shape = RoundedCornerShape(20),
+                            color = if (selectedRows[1]) Color(0xFF405E90) else Color.Transparent
+                        ),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row {
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            text = "Didanai Sebagian",
+                            fontWeight = FontWeight.Bold,
+                            color = if (selectedRows[1]) Color.White else Color.Black
+                        )
+                    }
+                    if (selectedRows[1]) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Icon Check",
+                                tint = Color.White
+                            )
+                        }
+                    }
+                }
+
                 // == AKHIR PILIHAN LEVEL PENDIDIKAN ==
             }
             Button(
