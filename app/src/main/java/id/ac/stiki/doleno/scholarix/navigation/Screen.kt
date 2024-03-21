57409c6e2bcee0a10b1abf7986a23edcb736133e
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-21 18:45:08 +0700

    Add sealed class Screen that contains route

---
+++ app/src/main/java/id/ac/stiki/doleno/scholarix/navigation/Screen.kt
@@ -0,0 +1,7 @@
+package id.ac.stiki.doleno.scholarix.navigation
+
+sealed class Screen(val route: String) {
+    object LoginScreen : Screen("login_screen")
+    object SignupScreen : Screen("signup_screen")
+    object LupaPasswordScreen : Screen("lupa_password_Screen")
+}
