9329eb25abef3d97a2a1564e3e091c3ad86dc058
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-05-07 14:58:07 +0700

    Update: Switch scholarship data source from dummy to live web data

--- app/src/main/java/id/ac/stiki/doleno/scholarix/view/main/HomeScreen.kt
+++ app/src/main/java/id/ac/stiki/doleno/scholarix/view/main/HomeScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -35,7 +34,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import id.ac.stiki.doleno.scholarix.model.Beasiswa
-import id.ac.stiki.doleno.scholarix.model.DummyBeasiswa
 import id.ac.stiki.doleno.scholarix.navigation.Screen
 import id.ac.stiki.doleno.scholarix.viewmodel.MainViewModel
 
@@ -78,187 +76,144 @@ fun HomeScreen(navController: NavController, viewModel: MainViewModel) {
                 )
             }
         }
-        LazyRow {
-            items(DummyBeasiswa.beasiswaList) { beasiswa ->
-                BeasiswaItem(beasiswa = beasiswa, navController)
+        when {
+            isLoading.value -> {
+                // Display a loading animation or indicator
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
             }
-        }
 
-        // TODO AWAL
-        // TODO Tolong bantu saya untuk menampilkan nama dari scholarship yg diambil dari json
-        // disini saya sudah buat kodingannya namun belum tampil
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .padding(start = 16.dp)
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = "Tes JSON",
-                fontWeight = FontWeight.Bold,
-                letterSpacing = (-0.1).sp
-            )
-            TextButton(onClick = { navController.navigate(Screen.KalenderBeasiswaScreen.route) }) {
-                Text(
-                    text = "Lihat Semua",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 12.sp,
-                    letterSpacing = (-0.1).sp,
-                    color = Color.Blue
+            isError.value -> {
+                // Show an error message and possibly a retry button
+                Text("Failed to load scholarships. Tap to retry.",
+                    modifier = Modifier
+                        .clickable { viewModel.fetchScholarshipDetails() }
+                        .padding(16.dp)
+                        .align(Alignment.CenterHorizontally))
+            }
+
+            else -> {
+                ScholarshipList(
+                    scholarships = scholarships.value,
+                    navController = navController
                 )
             }
         }
-        Column {
-
-        }
-        Column(modifier = Modifier.fillMaxSize()) {
-            when {
-                isLoading.value -> {
-                    // Display a loading animation or indicator
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-                }
-
-                isError.value -> {
-                    // Show an error message and possibly a retry button
-                    Text("Failed to load scholarships. Tap to retry.",
-                        modifier = Modifier
-                            .clickable { viewModel.fetchScholarshipDetails() }
-                            .padding(16.dp)
-                            .align(Alignment.CenterHorizontally))
-                }
-
-                else -> {
-                    ScholarshipList(scholarships = scholarships.value)
-                }
-            }
-        }
     }
 }
 
 @Composable
-fun ScholarshipList(scholarships: List<Beasiswa>) {
-    LazyColumn {
+fun ScholarshipList(scholarships: List<Beasiswa>, navController: NavController) {
+    LazyRow {
         items(scholarships) { scholarship ->
-            ScholarshipItem(scholarship = scholarship)
+            BeasiswaItem(beasiswa = scholarship, navController)
         }
     }
 }
-
-@Composable
-fun ScholarshipItem(scholarship: Beasiswa) {
-    Text(
-        text = scholarship.nama,
-        fontSize = 14.sp,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 18.sp,
-        modifier = Modifier
-            .heightIn(32.dp) // Set minimum height
-            .padding(8.dp) // Add some padding
-    )
-}
-
-// TODO AKHIR
 
 @Composable
 fun BeasiswaItem(beasiswa: Beasiswa, navController: NavController) {
-    val paddingValues = if (beasiswa.id == 0L) 16.dp else 8.dp
 
-    OutlinedCard(
-        modifier = Modifier
-            .padding(start = paddingValues, end = 8.dp)
-            .widthIn(max = 260.dp)
-            .height(175.dp)
-            .clickable { navController.navigate("${Screen.DetailBeasiswaScreen.route}/${beasiswa.id}") },
-        border = BorderStroke(1.dp, Color.LightGray),
-        shape = RoundedCornerShape(8.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.Transparent
-        ),
-    ) {
-        Column(
+    if (beasiswa.nama != "Title not found") {
+        val paddingValues = if (beasiswa.id == 0L) 16.dp else 8.dp
+
+        OutlinedCard(
             modifier = Modifier
-                .padding(12.dp)
-                .fillMaxHeight(),
-            verticalArrangement = Arrangement.SpaceBetween
+                .padding(start = paddingValues, end = 8.dp)
+                .widthIn(max = 260.dp)
+                .height(182.dp)
+                .clickable { navController.navigate("${Screen.DetailBeasiswaScreen.route}/${beasiswa.id}") },
+            border = BorderStroke(1.dp, Color.LightGray),
+            shape = RoundedCornerShape(8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Transparent
+            ),
         ) {
-            Column {
-                Row {
-                    // Menampilkan setiap derajat dalam kartu terpisah
-                    beasiswa.degrees.forEach { degree ->
-                        val containerColor = when (degree) {
-                            "S1" -> Color(0xFFD9FAE7)
-                            "S2" -> Color(0x401B73B3)
-                            "S3" -> Color(0x40C77738)
-                            else -> Color.Gray // Warna default jika derajat tidak dikenali
+            Column(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Row {
+                        // Menampilkan setiap derajat dalam kartu terpisah
+                        beasiswa.degrees.forEach { degree ->
+                            val containerColor = when (degree) {
+                                "S1" -> Color(0xFFD9FAE7)
+                                "S2" -> Color(0x401B73B3)
+                                "S3" -> Color(0x40C77738)
+                                else -> Color.Gray // Warna default jika derajat tidak dikenali
+                            }
+                            val textColor = when (degree) {
+                                "S1" -> Color(0xFF21764C) // Warna teks untuk S1
+                                "S2" -> Color(0xFF1B73B3) // Warna teks untuk S2
+                                "S3" -> Color(0xFFC77738) // Warna teks untuk S3
+                                else -> Color.Black // Warna teks default jika derajat tidak dikenali
+                            }
+                            Card(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = containerColor,
+                                )
+                            ) {
+                                Text(
+                                    text = degree,
+                                    modifier = Modifier.padding(
+                                        horizontal = 12.dp,
+                                        vertical = 4.dp
+                                    ),
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = textColor
+                                )
+                            }
+                            Spacer(modifier = Modifier.width(8.dp))
                         }
-                        val textColor = when (degree) {
-                            "S1" -> Color(0xFF21764C) // Warna teks untuk S1
-                            "S2" -> Color(0xFF1B73B3) // Warna teks untuk S2
-                            "S3" -> Color(0xFFC77738) // Warna teks untuk S3
-                            else -> Color.Black // Warna teks default jika derajat tidak dikenali
-                        }
+//                Spacer(modifier = Modifier.width(2.dp))
                         Card(
                             colors = CardDefaults.cardColors(
-                                containerColor = containerColor,
+                                containerColor = Color(0x80D9D9D9),
                             )
                         ) {
-                            Text(
-                                text = degree,
-                                modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
-                                fontSize = 12.sp,
-                                fontWeight = FontWeight.Bold,
-                                color = textColor
-                            )
-                        }
-                        Spacer(modifier = Modifier.width(8.dp))
-                    }
-//                Spacer(modifier = Modifier.width(2.dp))
-                    Card(
-                        colors = CardDefaults.cardColors(
-                            containerColor = Color(0x80D9D9D9),
-                        )
-                    ) {
-                        beasiswa.lokasi.kota?.let {
-                            Text(
-                                text = it,
-                                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
-                                fontSize = 12.sp,
-                                fontWeight = FontWeight.Bold,
-                                //                        color = Color(0xCC17181A)
-                            )
+                            beasiswa.lokasi.kota?.let {
+                                Text(
+                                    text = it,
+                                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    //                        color = Color(0xCC17181A)
+                                )
+                            }
                         }
                     }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = "Beasiswa ${beasiswa.pendanaan}", fontSize = 12.sp)
+                    Text(
+                        text = beasiswa.nama,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 18.sp,
+                        modifier = Modifier
+                            .heightIn(32.dp) // Set tinggi minimum
+                    )
                 }
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "Beasiswa ${beasiswa.pendanaan}", fontSize = 12.sp)
-                Text(
-                    text = beasiswa.nama,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold,
-                    lineHeight = 18.sp,
-                    modifier = Modifier
-                        .heightIn(32.dp) // Set tinggi minimum
-                )
-            }
-//            Spacer(modifier = Modifier.heightIn(16.dp))
-            // Kartu Deadline
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(0x80D9D9D9),
-                )
-            ) {
-                Row(
-                    modifier = Modifier
-                        .padding(vertical = 6.dp, horizontal = 12.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                Spacer(modifier = Modifier.heightIn(4.dp))
+                // Kartu Deadline
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0x80D9D9D9),
+                    )
                 ) {
-                    Text(text = "Deadline", fontSize = 12.sp)
-                    beasiswa.deadline?.let {
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 6.dp, horizontal = 12.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Deadline", fontSize = 12.sp)
+                        // Check if the deadline is null and choose the display text accordingly
                         Text(
-                            text = it,
+                            text = beasiswa.deadline ?: "N/A",  // Display "N/A" if deadline is null
                             fontSize = 12.sp,
                             color = Color.Red
                         )
